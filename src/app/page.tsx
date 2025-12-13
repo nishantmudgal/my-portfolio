2e074a4d886dd73375259dcc27ee3f3a2bf5b9a6
nishantmudgal <3nishantmudgal@gmail.com>
2025-12-13 22:42:28 +0530

    Initial Next.js setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen text-2xl font-semibold">
+      Basic Next.js App Deployed Successfully!
+    </main>
+  );
+}
